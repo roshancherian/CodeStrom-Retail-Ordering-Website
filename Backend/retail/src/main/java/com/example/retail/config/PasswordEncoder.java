ec2c2e2dcc4072eed329b6c3b7ebb12e0ac1022f
Harshini M <harshinivec@gmail.com>
2026-04-16 12:10:41 +0530

    Changes in config

--- Backend/retail/src/main/java/com/example/retail/config/PasswordEncoder.java
+++ Backend/retail/src/main/java/com/example/retail/config/PasswordEncoder.java
@@ -1,11 +1,15 @@
 package com.example.retail.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordEncoder {
     @Configuration
     public class SecurityConfig {
 
         @Bean
-        public PasswordEncoder passwordEncoder() {
+        public BCryptPasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
         }
     }
